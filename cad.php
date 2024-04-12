0595504157d466ba915a5cce662b76d6d48df5a0
Nayara Grippa <70596248+nayaragrippa@users.noreply.github.com>
2024-04-12 10:14:01 -0300

    Update cad.php

--- cad.php
+++ cad.php
@@ -17,24 +17,24 @@
 
     $conn = mysqli_connect("localhost", "root", "", "sampledb");
          
-    // Checando conexão
+    // Check connection
     if($conn === false){
         die("ERROR: Could not connect. "
             . mysqli_connect_error());
     }
  
-    // Pegando três valores do formulário data(input)
+    // Taking all 3 values from the form data(input)
     $nome =  $_REQUEST['nome'];
     $email = $_REQUEST['email'];
     $senha =  $_REQUEST['senha'];
 
     if($nome == "" or $email == "" or $senha == "") {
         echo "<h1>Dados incorretos ou faltantes, informe os dados corretamente.</h1>"
-        .("<button onclick= \"location.href='index.html'\">Voltar</button>");
+        .("<button onclick= \"location.href='index.php'\">Voltar</button>");
     }
     else {
-    // Realizando o insert query 
-    // Aqui o nome da nossa tabela é contactform_entries
+    // Performing insert query execution
+    // here our table name is contactform_entries
     $sql = "INSERT INTO contactform_entries  VALUES ('$nome', 
     '$email','$senha')";
 
